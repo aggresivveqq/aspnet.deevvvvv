3fd93a0f4bbdf421efa947396866d49fbda0ef8f
aggresivveqq <china02062004@mail.ru>
2024-11-16 23:41:29 +0500

    note api on asp

---
+++ NotesApi/Controllers/NoteController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotesApi.Models;
+
+namespace NoteApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : Controller
+    {
+        private static List<NoteModel> noteModels = new List<NoteModel>();
+        [HttpGet]
+        public IActionResult TakeAll()
+        {
+            return Ok(noteModels);
+        }
+        [HttpPost]
+        public IActionResult Add(NoteModel note)
+        {
+            note.Id = noteModels.Count + 1;
+            noteModels.Add(note);
+            return CreatedAtAction(nameof(TakeAll), new { id = note.Id }, note);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Edit(int id, NoteModel UpdatedNote)
+        {
+            var note = noteModels.FirstOrDefault(n => n.Id == id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+            note.Title = UpdatedNote.Title;
+            note.Content = UpdatedNote.Content;
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
+        {
+            var note = noteModels.FirstOrDefault(n => n.Id == id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+            noteModels.Remove(note);
+            return NoContent();
+
+        }
+    }
+}
---
+++ NotesApi/Models/NoteModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NotesApi.Models
+{
+    public class NoteModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+        [StringLength(500)]
+        public string Content { get; set; }
+    }
+}
